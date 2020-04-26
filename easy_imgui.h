aeefea197b9405c11d965ea52bf4ed0f0e678ae2
jon-dez <48642222+jon-dez@users.noreply.github.com>
2020-04-26 18:34:15 -0400

    Easy header

---
+++ easy_imgui.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "imgui/imgui.h"
+#include "Interface/ImGuiInterface.hpp"
+
+#ifdef EASY_HELPERS_UI
+#include "tools/ui_helpers.hpp"
+#endif
+
+#ifdef EASY_IMAGELOAD
+#include "tools/ImageLoad/ImageLoad.hpp"
+#endif
+
+#ifdef EASY_DIREXPLORER_UI
+#include "tools/DirExplorer/ImGuiDirExplorer.hpp"
+#endif
